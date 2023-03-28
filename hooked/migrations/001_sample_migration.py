a21f8e5c752b5ef0c6b30f0a1a2b19618e60e8b7
Jeff Chang <jeffjmc4@gmail.com>
2023-03-27 17:50:18 -0700

    changed sample-service to hooked, and set up database

--- hooked/migrations/001_sample_migration.py
+++ hooked/migrations/001_sample_migration.py
@@ -2,25 +2,22 @@ steps = [
     [
         # "Up" SQL statement
         """
-        CREATE TABLE dummy (
+        CREATE TABLE users (
             id SERIAL PRIMARY KEY NOT NULL,
-            required_limited_text VARCHAR(1000) NOT NULL,
-            required_unlimited_text TEXT NOT NULL,
-            required_date_time TIMESTAMP NOT NULL,
-            automatically_set_date_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-            required_integer INTEGER NOT NULL,
-            required_money MONEY NOT NULL
+            name VARCHAR(50) NOT NULL,
+            email VARCHAR(100) NOT NULL,
+            password VARCHAR(50) NOT NULL
         );
         """,
         # "Down" SQL statement
         """
-        DROP TABLE dummy;
+        DROP TABLE users;
         """
     ],
     [
         # "Up" SQL statement
         """
-        CREATE TABLE big_dummy (
+        CREATE TABLE location (
             id SERIAL PRIMARY KEY NOT NULL,
             required_limited_text VARCHAR(1000) NOT NULL,
             required_unlimited_text TEXT NOT NULL,
@@ -32,7 +29,25 @@ steps = [
         """,
         # "Down" SQL statement
         """
-        DROP TABLE big_dummy;
+        DROP TABLE location;
+        """
+    ],
+    [
+        # "Up" SQL statement
+        """
+        CREATE TABLE fish (
+            id SERIAL PRIMARY KEY NOT NULL,
+            required_limited_text VARCHAR(1000) NOT NULL,
+            required_unlimited_text TEXT NOT NULL,
+            required_date_time TIMESTAMP NOT NULL,
+            automatically_set_date_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+            required_integer INTEGER NOT NULL,
+            required_money MONEY NOT NULL
+        );
+        """,
+        # "Down" SQL statement
+        """
+        DROP TABLE fish;
         """
     ]
 ]
