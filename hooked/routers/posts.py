1a87da62e01ca0ed9e132cc0f2c923617c50ea0a
Sean <skoreancho@gmail.com>
2023-04-04 12:51:48 -0700

    protected endpoints for update post

--- hooked/routers/posts.py
+++ hooked/routers/posts.py
@@ -1,14 +1,40 @@
-from fastapi import APIRouter, Depends, Response
+from fastapi import APIRouter, Depends, Response, Request
 from typing import List, Optional, Union
 from authenticator import authenticator
+from jwtdown_fastapi.authentication import Token
 from queries.posts import (
     Error,
     PostIn,
     PostOut,
     PostRepository
 )
+from queries.users import UserOut
+
+class AccountToken(Token):
+    account: UserOut
 
 router = APIRouter()
+
+@router.get('/api/protected', response_model=bool)
+async def get_protected(
+    account_data: dict = Depends(authenticator.get_current_account_data)
+):
+    print(account_data)
+    return True
+
+
+@router.get("/token", response_model=AccountToken | None)
+async def get_token(
+    request: Request,
+    user: UserOut = Depends(authenticator.try_get_current_account_data)
+) -> AccountToken | None:
+    if authenticator.cookie_name in request.cookies:
+        return {
+            "access_token": request.cookies[authenticator.cookie_name],
+            "type": "Bearer",
+            "account": user,
+        }
+
 
 @router.post('/api/posts', response_model=Union[PostOut, Error])
 def create_post(
@@ -39,7 +65,8 @@ def update_post(
     post_id: int,
     post: PostIn,
     response: Response,
-    repo: PostRepository = Depends()
+    repo: PostRepository = Depends(),
+    account_data: dict = Depends(authenticator.get_current_account_data)
 ) -> Union[PostOut, Error]:
     response = repo.update_post(post_id, post)
     if not response:
