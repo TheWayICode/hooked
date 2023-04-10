2b0bb47fbe21b371984db139587088452c875612
Jeff Chang <jeffjmc4@gmail.com>
2023-04-09 17:03:56 -0700

    added FE logout feature

--- ghi/src/Components/NavLog/LoggedNav.js
+++ ghi/src/Components/NavLog/LoggedNav.js
@@ -1,7 +1,17 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import useToken from "@galvanize-inc/jwtdown-for-react";
 
 export const LoggedNav = () => {
+  const navigate = useNavigate();
+  const { logout } = useToken();
+
+  const handleLogout = (e) => {
+    e.preventDefault();
+    logout();
+    navigate("/");
+  };
+
   return (
     <nav className="sticky top-0 z-10 bg-[#0000005f] backdrop-filter backdrop-blur-lg bg-opacity-30">
       <div className="flex justify-between items-center h-20 max-w-[1240px] mx-auto px-4 text-white">
@@ -15,7 +25,7 @@ export const LoggedNav = () => {
           <li className="p-4 font-bold text-xl hover:text-[#2ddfb8]">Guides</li>
           <li className="p-4 font-bold text-xl hover:text-[#2ddfb8]">Forum</li>
           <li className="p-4 font-bold text-xl hover:text-[#29b798] text-[#2ddfb8]">
-            <button>Logout</button>
+            <button onClick={handleLogout}>Logout</button>
           </li>
         </ul>
       </div>
