9b8466d3504e7160bfa63d2bca0dc4b7cee2e5be
Sean <skoreancho@gmail.com>
2023-04-16 17:29:20 -0700

    created detail page for fishing locations

--- ghi/src/App.js
+++ ghi/src/App.js
@@ -4,7 +4,15 @@ import { AuthProvider } from "@galvanize-inc/jwtdown-for-react";
 import NavBar from "./Components/Navbar";
 import SignUpForm from "./Components/SignUpForm";
 import LoginForm from "./Components/LogInForm";
-import Santa_Monica_Pier_Details from "./Components/LocationDetails/SantaMonica";
+import Santa_Monica_Pier_Details from "./Components/LocationDetails/SantaMonicaPier";
+import Lake_Shasta_Details from "./Components/LocationDetails/LakeShasta";
+import Adirondack_Mountains_Details from "./Components/LocationDetails/AdirondackMountains";
+import Hudson_River_Details from "./Components/LocationDetails/HudsonRiver";
+import Lake_Okeechobee_Details from "./Components/LocationDetails/LakeOkeechobee";
+import Lake_Texoma_Details from "./Components/LocationDetails/LakeTexoma";
+import Lake_Travis_Details from "./Components/LocationDetails/LakeTravis";
+import Sebago_Lake_Details from "./Components/LocationDetails/SebagoLake";
+import Moosehead_Lake_Details from "./Components/LocationDetails/MooseheadLake";
 import Hero from "./Components/Hero";
 import Footer from "./Components/Footer";
 
@@ -19,7 +27,17 @@ function App() {
           <Route path="/" element={<Hero />} />
           <Route path="/signup" element={<SignUpForm />} />
           <Route path="/login" element={<LoginForm />} />
-          <Route path="/locations/Santa_Monica_Pier/" element={<Santa_Monica_Pier_Details />} />
+          <Route path="/locations">
+            <Route path="Santa_Monica_Pier" element={<Santa_Monica_Pier_Details />} />
+            <Route path="Adirondack_Mountains" element={<Adirondack_Mountains_Details />} />
+            <Route path="Hudson_River" element={<Hudson_River_Details />} />
+            <Route path="Lake_Okeechobee" element={<Lake_Okeechobee_Details />} />
+            <Route path="Lake_Shasta" element={<Lake_Shasta_Details />} />
+            <Route path="Lake_Texoma" element={<Lake_Texoma_Details />} />
+            <Route path="Lake_Travis" element={<Lake_Travis_Details />} />
+            <Route path="Moosehead_Lake" element={<Moosehead_Lake_Details />} />
+            <Route path="Sebago_Lake" element={<Sebago_Lake_Details />} />
+          </Route>
         </Routes>
         <Footer />
       </AuthProvider>
