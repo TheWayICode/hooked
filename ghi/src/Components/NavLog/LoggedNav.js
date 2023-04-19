27c47214f2d0cbe4d6377b5ea92c20d828ff9215
Jeff Chang <jeffjmc4@gmail.com>
2023-04-19 14:22:49 -0700

    added hamburger icon to make website responsive

--- ghi/src/Components/NavLog/LoggedNav.js
+++ ghi/src/Components/NavLog/LoggedNav.js
@@ -1,10 +1,17 @@
 import React from "react";
 import { Link, useNavigate } from "react-router-dom";
+import { useState } from "react";
 import useToken from "@galvanize-inc/jwtdown-for-react";
+import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
 
 export const LoggedNav = () => {
   const navigate = useNavigate();
   const { logout } = useToken();
+
+  const [nav, setNav] = useState(true);
+  const handleNav = () => {
+    setNav(!nav);
+  };
 
   const handleLogout = (e) => {
     e.preventDefault();
@@ -26,7 +33,7 @@ export const LoggedNav = () => {
           </span>
         </h1>
 
-        <ul className="flex">
+        <ul className="md:flex hidden">
           <li className="p-4 font-bold text-xl hover:text-[#2ddfb8]">
             <a href="/searchpage">Search</a>
           </li>
@@ -43,6 +50,40 @@ export const LoggedNav = () => {
             <button onClick={handleLogout}>Logout</button>
           </li>
         </ul>
+        <div
+          onClick={handleNav}
+          className="md:hidden pt-2 flex justify-end pr-5"
+        >
+          <AiOutlineMenu size={30} />
+        </div>
+        <div
+          className={
+            nav
+              ? "fixed top-0 right-0 w-[40%] h-full bg-[#000] ease-in-out duration-500 transition-all"
+              : "hidden"
+          }
+        >
+          <div onClick={handleNav} className="flex justify-end pr-11 pt-7">
+            <AiOutlineClose size={30} />
+          </div>
+          <ul className="bg-[#000] h-screen pl-3">
+            <li className="p-4 font-bold text-2xl hover:text-[#2ddfb8]">
+              <a href="/searchpage">Search</a>
+            </li>
+            <li className="p-4 font-bold text-2xl hover:text-[#2ddfb8]">
+              <a href="/guides">Guides</a>
+            </li>
+            <li className="p-4 font-bold text-2xl hover:text-[#2ddfb8]">
+              <a href="/forum">Forum</a>
+            </li>
+            <li className="p-4 font-bold text-2xl hover:text-[#2ddfb8]">
+              <a href="/users">Profile</a>
+            </li>
+            <li className="p-4 font-bold text-2xl hover:text-[#29b798] text-[#2ddfb8]">
+              <button onClick={handleLogout}>Logout</button>
+            </li>
+          </ul>
+        </div>
       </div>
     </nav>
   );
--- ghi/src/Components/searchPageInfoCards/newsUpdate.js
+++ ghi/src/Components/searchPageInfoCards/newsUpdate.js
@@ -9,7 +9,7 @@ const NewsUpdate = () => {
         <h1 className="md:text-5xl sm:text-4xl text-3xl text-black font-bold py-4 pb-6">
           Recent Events & Fishing News
         </h1>
-        <div className="flex justify-between items-center gap-8">
+        <div className="flex justify-between items-center gap-12">
           <div className="max-w-[400px] max-h-[800px] mx-auto">
             <img src={image} alt="newspic" />
             <h1 className="md:text-2xl sm:text-xl text-l font-bold pt-3">
@@ -64,7 +64,7 @@ const NewsUpdate = () => {
               Read More &gt;&gt;
             </Link>
           </div>
-          <div className="max-w-[400px] m-auto">
+          {/* <div className="max-w-[400px] m-auto">
             <img src={image} alt="newspic" />
             <h1 className="md:text-2xl sm:text-xl text-l font-bold pt-3">
               5-Minute Tilapia Recipe
@@ -81,7 +81,7 @@ const NewsUpdate = () => {
             >
               Read More &gt;&gt;
             </Link>
-          </div>
+          </div> */}
         </div>
       </div>
     </div>
