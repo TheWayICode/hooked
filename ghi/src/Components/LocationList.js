48081be540c1326332a793f8bb64eaec4e9ae3f2
Jeff Chang <jeffjmc4@gmail.com>
2023-04-20 18:26:25 -0700

    added a page for no search results

--- ghi/src/Components/LocationList.js
+++ ghi/src/Components/LocationList.js
@@ -33,30 +33,38 @@ const LocationList = () => {
         <h1 className="text-7xl font-bold text-center mt-10 mb-5">
           Search Results
         </h1>
-        <div className="max-w-[840px] flex text-center mx-auto p-5">
-          {filteredData.map((state) => {
-            return (
-              <div
-                key={state.id}
-                className="p-3 bg-[#effdff] max-w-[250px] max-h-[250px] text-left"
-              >
-                <img
-                  src={state.picture_url}
-                  alt={state.name}
-                  className="w-full h-[200px] object-cover rounded-md shadow-xl"
-                />
-                <h2 className="text-2xl font-bold py-1">{state.city}</h2>
-                <h4 className="text-base font-medium">{state.name}</h4>
-                <Link
-                  to={`/locations/${state.name.split(" ").join("_")}`}
-                  className="text-sm text-[#00df9a] hover:text-black"
+        {filteredData.length > 0 ? (
+          <div className="max-w-[840px] flex text-center mx-auto p-5">
+            {filteredData.map((state) => {
+              return (
+                <div
+                  key={state.id}
+                  className="p-3 bg-[#effdff] max-w-[250px] max-h-[250px] text-left"
                 >
-                  Go to Page
-                </Link>
-              </div>
-            );
-          })}
-        </div>
+                  <img
+                    src={state.picture_url}
+                    alt={state.name}
+                    className="w-full h-[200px] object-cover rounded-md shadow-xl"
+                  />
+                  <h2 className="text-2xl font-bold py-1">{state.city}</h2>
+                  <h4 className="text-base font-medium">{state.name}</h4>
+                  <Link
+                    to={`/locations/${state.name.split(" ").join("_")}`}
+                    className="text-sm text-[#00df9a] hover:text-black"
+                  >
+                    Go to Page
+                  </Link>
+                </div>
+              );
+            })}
+          </div>
+        ) : (
+          <div className="text-center">
+            <h1 className="mt-10 text-2xl font-semibold text-center">
+              Uh oh! No locations, I think you're lost...
+            </h1>
+          </div>
+        )}
         <div className="flex justify-end">
           <img
             src={image}
