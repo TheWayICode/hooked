e3b4e6aff46f5b04108338fd5fc3f0324322b837
Sean <skoreancho@gmail.com>
2023-04-25 12:51:03 -0700

    testing mapbox api key on Santa Monica Pier

--- ghi/src/Components/LocationDetails/SantaMonicaPier.js
+++ ghi/src/Components/LocationDetails/SantaMonicaPier.js
@@ -18,14 +18,14 @@ function SantaMonicaPierDetails() {
 
   const fishList = async () => {
     const fishUrl =
-      "pk.eyJ1IjoiYnVycml0b2JveWNobzMiLCJhIjoiY2xnZW00dDdsMm1neDNtbzgzdHA5ZTA2aiJ9.x_k3x1B_c9Htw7jgUzCn0A";
+      `${process.env.REACT_APP_USER_SERVICE_API_HOST}/api/locations/1`;
     const fishResponse = await fetch(fishUrl);
     if (fishResponse.ok) {
       const fishData = await fishResponse.json();
       setFish(fishData.fish);
       setDescription(fishData.description);
 
-      mapboxgl.accessToken = `${process.env.MAPBOX_API_KEY}`;
+      mapboxgl.accessToken = "pk.eyJ1IjoiYnVycml0b2JveWNobzMiLCJhIjoiY2xnZW00dDdsMm1neDNtbzgzdHA5ZTA2aiJ9.x_k3x1B_c9Htw7jgUzCn0A";
       const map = new mapboxgl.Map({
         container: "map",
         style: "mapbox://styles/mapbox/streets-v11",
